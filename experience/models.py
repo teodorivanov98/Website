916934f88e147bd0789e354b046cdb7c7b883b42
Teodor Ivanov <teodor.ivanov.2498@gmail.com>
2025-05-20 17:10:05 +0300

    Adding the structure

---
+++ experience/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+class Job(models.Model):
+    company = models.CharField(max_length=100)
+    position = models.CharField(max_length=100)
+    start_date = models.DateField()
+    end_date = models.DateField(null=True, blank=True)
+    description = models.TextField(blank=True)
+
+    def __str__(self):
+        return f"{self.position} at {self.company}"
