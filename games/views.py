458221099fe8f7da46e9f3f7c7380134d55e4e1b
Teodor Ivanov <teodor.ivanov.2498@gmail.com>
2025-05-21 20:55:22 +0300

    Updating

--- games/views.py
+++ games/views.py
@@ -1,15 +1,13 @@
 from django.shortcuts import render
 
-# Create your views here.
-from django.shortcuts import render
-
 def home(request):
     return render(request, 'games/home.html')
+
 def tictactoe(request):
     return render(request, 'games/tictactoe.html')
 
-def rps(request):
-    return render(request, 'games/rps.html')
+def rock_paper_scissors(request):
+    return render(request, 'games/rock_paper_scissors.html')
 
 def guess_animal(request):
     return render(request, 'games/guess_animal.html')
