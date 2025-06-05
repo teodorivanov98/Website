d4782f6e5174a46aa4c8831e4fc50481d6e7ded5
Teodor Ivanov <teodor.ivanov.2498@gmail.com>
2025-06-05 12:58:03 +0300

    Finalizing

--- contacts/views.py
+++ contacts/views.py
@@ -9,19 +9,19 @@ def home(request):
         email = request.POST.get('email')
         message = request.POST.get('message')
 
-        # Save the message to the database
+
         Contact.objects.create(name=name, email=email, message=message)
 
-        # Send the message via email
+
         send_mail(
             subject=f'New contact message from {name}',
             message=message,
             from_email=settings.DEFAULT_FROM_EMAIL,
-            recipient_list=[settings.EMAIL_HOST_USER],  # Your email address
+            recipient_list=[settings.EMAIL_HOST_USER],
             fail_silently=False,
         )
 
-        return redirect('contacts-home')  # Or use a success page if you prefer
+        return redirect('contacts-home')
 
     # Show past messages below the form
     contacts = Contact.objects.all().order_by('-id')
